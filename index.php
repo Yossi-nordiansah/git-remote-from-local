baad1ad12bc0d8c43e2707024b584b9277b6978b
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2023-09-04 11:37:52 +0700

    add list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, atque.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
